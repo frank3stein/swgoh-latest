9a36421d6119abfd5e9d82561591f84ce05b6c0e
Emil <martinov.emil@gmail.com>
2024-05-23 11:12:41 +0300

    format 2024

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { promises as fs } from 'fs';
 
-export default function Home() {
+
+export default async function Page() {
+  const file = await fs.readFile(process.cwd() + '/app/data/characters.json', 'utf8');
+  const data = JSON.parse(file);
+  console.log(data[11].Image)
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xli w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
@@ -24,6 +29,8 @@ export default function Home() {
               height={24}
               priority
             />
+            <Image src={data[11].Image} alt="Vercel Logo" width={100} height={24} priority />
+            {/* <Image src={'/images/aaylasecura.png'} alt="Vercel Logo" width={100} height={24} priority /> */}
           </a>
         </div>
       </div>
